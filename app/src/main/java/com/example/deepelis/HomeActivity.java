47ffca12829e8a43a42017d549e382afcfb2ba9a
William1405 <61747355+William1405@users.noreply.github.com>
2021-04-23 18:13:15 -0500

    Implementación peliculas

--- app/src/main/java/com/example/deepelis/HomeActivity.java
+++ app/src/main/java/com/example/deepelis/HomeActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.widget.TableLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabLayout;
@@ -20,6 +22,7 @@ public class HomeActivity extends AppCompatActivity {
     private List<Slide> lstSlides;
     private ViewPager sliderpager;
     private TabLayout indicator;
+    private RecyclerView MoviesRV;
 
 
 
@@ -27,34 +30,34 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
-          sliderpager = findViewById(R.id.slider_page);
-          indicator=findViewById(R.id.indicator);
-
-
-
-
-
-
-
+        sliderpager = findViewById(R.id.slider_page);
+        indicator=findViewById(R.id.indicator);
+        MoviesRV =findViewById(R.id.Rv_movies);
         lstSlides = new ArrayList<>();
         lstSlides.add(new Slide(R.drawable.slide1, "SPACE JUM 2"));
         lstSlides.add(new Slide(R.drawable.slide2, "RAYA  AND THE LAST DRAGON"));
         lstSlides.add(new Slide(R.drawable.slide3, "TOM Y JERRY"));
-        
-
         SliderPagerAdapter adapter = new SliderPagerAdapter(this,lstSlides);
-
         sliderpager.setAdapter((adapter));
-
-
         //setup time
-
         Timer timer =new Timer();
         timer.scheduleAtFixedRate(new HomeActivity.SliderTimer(), 4000, 6000);
-
-
         indicator.setupWithViewPager(sliderpager,true);
+        //Recyclerview de peliculas
+
+        List<Movie> lstMovies= new ArrayList<>();
+        lstMovies.add(new Movie("King vs Godzilla", R.drawable.king) );
+        lstMovies.add(new Movie("Croods2", R.drawable.croods) );
+        lstMovies.add(new Movie("Liga de la justicia", R.drawable.liga));
+
+        MovieAdapter movieAdapter = new MovieAdapter(this,lstMovies);
+        MoviesRV.setAdapter(movieAdapter);
+        MoviesRV.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL, false));
+
+
+
+
+
 
 
 
